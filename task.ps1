6ad2cbb3b62e67038e8828f14c201b4501a1c7f1
Serhii <spridoloba@gmail.com>
2026-03-26 22:56:39 +0200

    feat: solution for task

--- task.ps1
+++ task.ps1
@@ -1,1 +1,14 @@
-# Write your code here
+$targetVmSize = 'Standard_B2pts_v2'
+$scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Path
+$dataDirectory = Join-Path $scriptDirectory 'data'
+$resultPath = Join-Path $scriptDirectory 'result.json'
+
+$regionsWithVmSize = foreach ($file in Get-ChildItem -Path $dataDirectory -Filter '*.json' -File) {
+    $vmSizes = Get-Content -Path $file.FullName -Raw | ConvertFrom-Json
+
+    if ($vmSizes | Where-Object { $_.Name -eq $targetVmSize }) {
+        $file.BaseName
+    }
+}
+
+$regionsWithVmSize | ConvertTo-Json | Set-Content -Path $resultPath
